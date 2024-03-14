7528a68e8ecd29ba408bce48cddd6cbca031f993
Styaaa <s160421048@student.ubaya.ac.id>
2024-03-14 16:15:41 +0800

    init

---
+++ app/src/main/java/com/satya/advweek4_160421048/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.satya.advweek4_160421048
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
